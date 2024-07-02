001a00f859c9c3c8c03df69eaff92317f034e67c
Jéssica Bom <jessica.mlb@gmail.com>
2024-07-02 17:35:40 -0300

    Código pós aula 01/07/24

---
+++ exercicios/para-sala/olaMundo.py
@@ -0,0 +1,8 @@
+import print_reprograma as pr
+
+pr.cor("magenta")
+pr.imprimir("Olá, mundo!")
+pr.separador(30, cor_texto="azul")
+pr.retangulo("Ain ti fofinhooooo", cor_barra="amarelo", cor_texto="vermelho")
+pr.limpar_formatacao()
+

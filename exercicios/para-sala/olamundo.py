1b73b3ca74a00fb71a24b4533d908729e47d512b
Guilhermina Pereira da Silva <mina@velicastelo.art>
2024-07-05 13:08:20 +0100

    Entrega de trabalho

---
+++ exercicios/para-sala/olamundo.py
@@ -0,0 +1,8 @@
+import print_reprograma as pr
+
+
+pr.cor('magenta')
+pr.imprimir("Olá, mundo!")
+pr.limpar_formatacao()
+pr.separador(11, cor_texto = 'azul')
+pr.retangulo('VELICASTELO', cor_barra = 'magenta', cor_texto = 'azul')

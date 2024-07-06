8a07ee42074905bcf9e00ba218b6e4571f4616ab
Ana Beatriz Figueiredo <anabiafigue01@gmail.com>
2024-07-05 20:43:22 -0400

    exercicio

---
+++ exercicios/para-sala/olá-mundo.py
@@ -0,0 +1,6 @@
+import print_reprograma as pr
+pr.cor('verde')
+pr.imprimir('olá, mundo!')
+pr.separador(30, cor_texto= "magenta")
+pr.retangulo('Bom dia, pessoal!', cor_barra="amarelo", cor_texto='vermelho')
+pr.limpar_formatacao()
